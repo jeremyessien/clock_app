4d5827f69f35138f9ad101f24351b7a584536b78
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-01 15:31:03 +0100

    Update clock_view.dart

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -24,13 +24,16 @@ class _ClockViewState extends State<ClockView> {
 }
 
 class ClockPainter extends CustomPainter {
+  var dateTime = DateTime.now();
   @override
   void paint(Canvas canvas, Size size) {
     var cX = size.width / 2;
     var cY = size.height / 2;
     var center = Offset(cX, cY);
     var radius = min(cX, cY);
+
     var fillBrush = Paint()..color = kFillBrushColor;
+
     var outlineBrush = Paint()
       ..color = kOutlineBrushColor
       ..strokeWidth = 16
@@ -44,30 +47,38 @@ class ClockPainter extends CustomPainter {
     var secondHand = Paint()
       ..color = Colors.orange.shade300
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 8
+      ..strokeWidth = 5
       ..style = PaintingStyle.stroke;
 
     var minutesHand = Paint()
       ..shader = RadialGradient(colors: [kMinutesHandStart, kMinutesHandEnd]).createShader(Rect.fromCircle(center: center, radius: radius))
-      ..strokeWidth = 16
+      ..strokeWidth = 8
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
     var hourHand = Paint()
       ..shader = RadialGradient(colors: [kHourHandStart, kHourHandEnd]).createShader(Rect.fromCircle(center: center, radius: radius))
-      ..strokeWidth = 16
+      ..strokeWidth = 12
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
-    var secondHandX = cX + 80 * cos(90 * pi/180);
-    var secondHandY = cX + 80 * sin(90 * pi/180);
 
     canvas.drawCircle(center, radius - 40, fillBrush);
     canvas.drawCircle(center, radius - 40, outlineBrush);
+
+    var hourHandX = cX + 60 * cos((dateTime.hour *30 + dateTime.minute*0.5) * pi/180);
+    var hourHandY = cX + 60 * sin((dateTime.hour *30 + dateTime.minute*0.5) * pi/180);
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHand);
+
+    var minutesHandX = cX + 80 * cos(dateTime.minute *6 * pi/180);
+    var minutesHandY = cX + 80 * sin(dateTime.minute *6 * pi/180);
+    canvas.drawLine(center, Offset(minutesHandX, minutesHandY), minutesHand);
+
+    var secondHandX = cX + 80 * cos(dateTime.second *6* pi/180);
+    var secondHandY = cX + 80 * sin(dateTime.second *6 * pi/180);
     canvas.drawLine(center, Offset(secondHandX, secondHandY), secondHand);
-    canvas.drawLine(center, Offset(160, 100), minutesHand);
-    canvas.drawLine(center, Offset(200, 100), hourHand);
-    canvas.drawCircle(center, 16, centerFillBrush);
+
+    canvas.drawCircle(center, 8, centerFillBrush);
 
 
 
