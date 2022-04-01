ccd10e60b55feaec57ab337686f5c0207f8ed7ba
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-01 13:53:30 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:clock_app/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Clock App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
