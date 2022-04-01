ad7cef0e353179e4fa5dc2005f5febee94f3170f
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-01 13:46:06 +0100

    Update clock_view.dart

---
+++ lib/clock_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ClockView extends StatefulWidget {
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: sized_box_for_whitespace
+    return Container(
+      width: 300,
+      height: 300,
+      // ignore: prefer_const_constructors
+      child: CustomPaint(
+        painter: ClockPainter(),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
