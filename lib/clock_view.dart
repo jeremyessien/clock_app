959149de80e191ec77cd311fafbf722eacec13b0
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-01 14:54:22 +0100

    Update clock_view.dart

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -1,9 +1,9 @@
-import 'dart:math';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
+import 'dart:math';
 import 'package:clock_app/constants.dart';
 import 'package:flutter/material.dart';
 
-// ignore: use_key_in_widget_constructors
 class ClockView extends StatefulWidget {
   @override
   _ClockViewState createState() => _ClockViewState();
@@ -16,7 +16,6 @@ class _ClockViewState extends State<ClockView> {
     return Container(
       width: 300,
       height: 300,
-      // ignore: prefer_const_constructors
       child: CustomPaint(
         painter: ClockPainter(),
       ),
@@ -44,19 +43,34 @@ class ClockPainter extends CustomPainter {
     
     var secondHand = Paint()
       ..color = Colors.orange.shade300
+      ..strokeCap = StrokeCap.round
       ..strokeWidth = 13
       ..style = PaintingStyle.stroke;
 
     var minutesHand = Paint()
-      // ignore: prefer_const_constructors, prefer_const_literals_to_create_immutables
       ..shader = RadialGradient(colors: [Colors.lightBlue, Colors.pink]).createShader(Rect.fromCircle(center: center, radius: radius))
       ..strokeWidth = 13
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+
+    var hourHand = Paint()
+      ..shader = RadialGradient(colors: [Colors.lightBlue, Colors.pink]).createShader(Rect.fromCircle(center: center, radius: radius))
+      ..strokeWidth = 13
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
     
 
     canvas.drawCircle(center, radius - 40, fillBrush);
     canvas.drawCircle(center, radius - 40, outlineBrush);
+    canvas.drawLine(center, Offset(100, 100), secondHand);
+    canvas.drawLine(center, Offset(100, 100), minutesHand);
+    canvas.drawLine(center, Offset(100, 100), hourHand);
+
     canvas.drawCircle(center, 16, centerFillBrush);
+
+
+
+
 
 
   }
