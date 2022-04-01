7fe9eef2cf6cfff9599b5f7aa46a1f55254de357
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-01 23:05:56 +0100

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,76 +11,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+
   DateTime now = DateTime.now();
   var formattedTime = DateFormat('HH:mm').format(DateTime.now());
   var formattedDate = DateFormat('EE, d MMM').format(DateTime.now());
+  var timeZone = DateTime.now().timeZoneOffset.toString().split('.').first;
+  var offsetSign = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kContainerBackground,
-      body: Container(
-        padding: EdgeInsets.all(32),
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Clock',
-              style: TextStyle(
-                color: kOutlineBrushColor,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              formattedTime,
-              style: TextStyle(
-                color: kOutlineBrushColor,
-                fontSize: 60,
-              ),
-            ),
-            Text(
-              formattedDate,
-              style: TextStyle(
-                color: kOutlineBrushColor,
-                fontSize: 20,
-              ),
-            ),
-            ClockView(),
-            Text(
-              'Timezone',
-              style: TextStyle(
-                color: kOutlineBrushColor,
-                fontSize: 24,
-              ),
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            Row(
+      body: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(32),
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.language,
-                  color: kOutlineBrushColor,
+                Text(
+                  'Clock',
+                  style: TextStyle(
+                    color: kOutlineBrushColor,
+                    fontSize: 20,
+                  ),
                 ),
                 SizedBox(
-                  width: 15,
+                  height: 30,
                 ),
                 Text(
-                  'UTC',
+                  formattedTime,
+                  style: TextStyle(
+                    color: kOutlineBrushColor,
+                    fontSize: 60,
+                  ),
+                ),
+                Text(
+                  formattedDate,
+                  style: TextStyle(
+                    color: kOutlineBrushColor,
+                    fontSize: 20,
+                  ),
+                ),
+                ClockView(),
+                Text(
+                  'Timezone',
                   style: TextStyle(
                     color: kOutlineBrushColor,
                     fontSize: 24,
                   ),
                 ),
+                SizedBox(
+                  width: 15,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.language,
+                      color: kOutlineBrushColor,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'UTC'  +  offsetSign + timeZone,
+                      style: TextStyle(
+                        color: kOutlineBrushColor,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
